caf45a56b4fc4ba91c2b2dae56d3a89bad4057d5
ayush <ayushjha0004@gmail.com>
2026-04-09 22:03:28 +0530

    feat: add custom disk-full task + register in UI

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -4,11 +4,13 @@ tasks/__init__.py - Task registry for SRE OpenEnv
 from tasks.task_easy import TASK_EASY
 from tasks.task_medium import TASK_MEDIUM
 from tasks.task_hard import TASK_HARD
+from tasks.task_custom import TASK_CUSTOM
 
 TASK_REGISTRY = {
     "easy": TASK_EASY,
     "medium": TASK_MEDIUM,
     "hard": TASK_HARD,
+    "custom": TASK_CUSTOM,
 }
 
-__all__ = ["TASK_REGISTRY", "TASK_EASY", "TASK_MEDIUM", "TASK_HARD"]
+__all__ = ["TASK_REGISTRY", "TASK_EASY", "TASK_MEDIUM", "TASK_HARD", "TASK_CUSTOM"]
